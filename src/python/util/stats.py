fd18716daa03860fc15775911c7837a94bf4ac09
artitzco <artitzco.data@gmail.com>
2023-10-21 00:46:55 -0600

    sugerencia gráficas

--- src/python/util/stats.py
+++ src/python/util/stats.py
@@ -1,19 +1,18 @@
-from src.python.util.basic import load_arrow
-import matplotlib.pyplot as plt
+import pandas as pd
 import numpy as np
 import random
 
 
-def boxplot(data):
-    q1 = np.percentile(data, 25)  # Primer cuartil
-    q2 = np.percentile(data, 50)  # Mediana
-    q3 = np.percentile(data, 75)  # Tercer cuartil
+def boxplot(src):
+    q1 = np.percentile(src, 25)  # Primer cuartil
+    q2 = np.percentile(src, 50)  # Mediana
+    q3 = np.percentile(src, 75)  # Tercer cuartil
     iqr = q3 - q1  # Rango intercuantil
     lower_bound = q1 - 1.5 * iqr  # Límite inferior
     upper_bound = q3 + 1.5 * iqr  # Límite superior
     bias = "none"
-    left = any(x < lower_bound for x in data)
-    right = any(x > upper_bound for x in data)
+    left = any(x < lower_bound for x in src)
+    right = any(x > upper_bound for x in src)
     if left and right:
         bias = "both"
     elif left:
@@ -23,7 +22,27 @@ def boxplot(data):
     return {
         "q1": q1, "q2": q2, "q3": q3, "iqr": iqr,
         "inf": lower_bound, "sup": upper_bound,
-        "bias": bias
+        "bias": bias, "left": left, 'right': right
+    }
+
+
+def confidence_range(src, level=100):
+    box = boxplot(src)
+    alpha = 1 - level / 100.0
+    if box['left'] and not box['right']:
+        return np.quantile(src, [alpha, 1])
+    if box['right'] and not box['left']:
+        return np.quantile(src, [0, 1 - alpha])
+    return np.quantile(src, [alpha / 2, 1 - alpha / 2])
+
+
+def histplot(src, level=100):
+    box = confidence_range(src, level=level)
+    src = src[(src >= box[0]) & (src <= box[1])]
+    bins = 1 + int(np.log2(len(src)))  # Regla de Sturges
+    return {
+        'serie': src,
+        'bins': bins
     }
 
 
@@ -33,30 +52,15 @@ def sample(src, level, deep=1):
     size = int(size * deep)
     min = window - 1
     max = len(src) - 1
-    index = sorted([int(random.uniform(min, max))for _ in range(size)])
-    sample = [src[i] for i in index]
-    mean = [src[i-window+1:i+1].mean() for i in index]
-    std = [src[i-window+1:i+1].std() for i in index]
+    index = pd.Series(sorted([int(random.uniform(min, max))for _ in range(size)]))
+    sample = pd.Series([src[i] for i in index])
+    mean = pd.Series([src[i-window+1:i+1].mean() for i in index])
+    std = pd.Series([src[i-window+1:i+1].std() for i in index])
     return {
         'index': index,
-        'sample': sample,
+        'serie': sample,
         'mean': mean,
         'std': std,
         'size': size,
         'window': window
     }
-
-
-interval = '30m'
-file = 'data/02 clean'
-name = f'BTCUSDT_{interval}'
-data = load_arrow(file, name)
-data
-
-serie = data['Close']
-
-serie.hist(bins=5, edgecolor='k')
-plt.xlabel('Valor')
-plt.ylabel('Frecuencia')
-plt.title('Histograma')
-plt.show()
